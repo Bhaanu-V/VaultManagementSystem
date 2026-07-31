8d30f05e695a3c11a4ab0fb10fe1f3452f63e8c5
Bhaanu-V <bhaanav@gmail.com>
2026-07-31 22:47:49 +0530

    file has been created

---
+++ Vault.java
@@ -0,0 +1,8 @@
+ import java.util.ArrayList;
+public class Vault{
+    ArrayList<Account> accounts = new ArrayList<>();
+    public void addAccount(Account acc) {
+        accounts.add(acc);
+    }
+}
+
