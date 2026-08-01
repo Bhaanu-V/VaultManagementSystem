8a9a162825d8deb49b443f1c4a05d2c779ba52e0
Bhaanu-V <bhaanav@gmail.com>
2026-08-01 21:51:25 +0530

    accountholder details and deposit operations has been created

--- Main.java
+++ Main.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bank Application Started");
+
+        Vault bank = new Vault();
+        Scanner sc = new Scanner(System.in);
+
+        // Create Account
+        System.out.print("Enter ID: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Balance: ");
+        double balance = sc.nextDouble();
+
+        bank.createAccount(id, name, balance);
+
+        // Deposit
+        System.out.print("Enter deposit amount: ");
+        double amount = sc.nextDouble();
+
+        bank.deposit(id, amount);
+        sc.close();
     }
 }
-
--- Vault.java
+++ Vault.java
@@ -1,8 +1,40 @@
  import java.util.ArrayList;
 public class Vault{
     ArrayList<Account> accounts = new ArrayList<>();
-    public void addAccount(Account acc) {
-        accounts.add(acc);
+    
+public void createAccount(int id, String name, double balance) {
+
+    for (Account acc : accounts) {
+        if (acc.getAccountId() == id) {
+            System.out.println("Duplicate Account ID not allowed");
+            return;
+        }
     }
+
+    Account newAcc = new Account(id, name, balance);
+    accounts.add(newAcc);
+
+    System.out.println("Account created successfully");
 }
+
+
+// DAY 5 - DEPOSIT
+public void deposit(int id, double amount) {
+
+    for (Account acc : accounts) {
+        if (acc.getAccountId() == id) {
+
+            double newBalance = acc.getBalance() + amount;
+
+            System.out.println("Amount deposited successfully");
+            System.out.println("New Balance: " + newBalance);
+
+            return;
+        }
+    }
+
+    System.out.println("Account not found");
+}
+    }
+
 
