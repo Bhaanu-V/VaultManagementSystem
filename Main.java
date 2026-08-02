e4149f521bd0a0f3263c02ded17db16feca8173f
Bhaanu-V <bhaanav@gmail.com>
2026-08-02 15:09:09 +0530

    accountcreated, withdraw, deposit and checkbalance has been updated successfully

--- Main.java
+++ Main.java
@@ -1,29 +1,72 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
         Vault bank = new Vault();
         Scanner sc = new Scanner(System.in);
 
-        // Create Account
-        System.out.print("Enter ID: ");
-        int id = sc.nextInt();
-        sc.nextLine();
+        while (true) {
 
-        System.out.print("Enter Name: ");
-        String name = sc.nextLine();
+            System.out.println("\n--- BANK MENU ---");
+            System.out.println("1. Create Account");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Check Balance");
+            System.out.println("5. Exit");
 
-        System.out.print("Enter Balance: ");
-        double balance = sc.nextDouble();
+            System.out.print("Enter choice: ");
+            int choice = sc.nextInt();
 
-        bank.createAccount(id, name, balance);
+            switch (choice) {
 
-        // Deposit
-        System.out.print("Enter deposit amount: ");
-        double amount = sc.nextDouble();
+                case 1:
+                    System.out.print("Enter ID: ");
+                    int id = sc.nextInt();
+                    sc.nextLine();
 
-        bank.deposit(id, amount);
-        sc.close();
+                    System.out.print("Enter Name: ");
+                    String name = sc.nextLine();
+
+                    System.out.print("Enter Balance: ");
+                    double balance = sc.nextDouble();
+
+                    bank.createAccount(id, name, balance);
+                    break;
+
+                case 2:
+                    System.out.print("Enter ID: ");
+                    int did = sc.nextInt();
+
+                    System.out.print("Enter Amount: ");
+                    double damt = sc.nextDouble();
+
+                    bank.deposit(did, damt);
+                    break;
+
+                case 3:
+                    System.out.print("Enter ID: ");
+                    int wid = sc.nextInt();
+
+                    System.out.print("Enter Amount: ");
+                    double wamt = sc.nextDouble();
+
+                    bank.withdraw(wid, wamt);
+                    break;
+
+                case 4:
+                    System.out.print("Enter ID: ");
+                    int cid = sc.nextInt();
+
+                    bank.checkBalance(cid);
+                    break;
+
+                case 5:
+                    System.out.println("Thank you!");
+                    sc.close();
+                    System.exit(0);
+            }
+        }
     }
 }
